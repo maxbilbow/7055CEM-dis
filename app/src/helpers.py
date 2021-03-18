5c83b2175746777a00c3e7654865ce97b6ae77d4
Max <maxbilbow@gmail.com>
2021-03-18 15:46:36 +0000

    Successful prediction (93%)
    Test data generated and removed features (92% success)

--- app/src/helpers.py
+++ app/src/helpers.py
@@ -9,3 +9,40 @@ def flatten(list_list: list, predicate: Callable[[object], bool] = lambda o: Tru
                 flat_list.append(item)
 
     return flat_list
+
+
+def find_correlation(data, threshold=0.9, remove_negative=False):
+    """
+    Given a numeric pd.DataFrame, this will find highly correlated features,
+    and return a list of features to remove.
+    Parameters
+    -----------
+    data : pandas DataFrame
+        DataFrame
+    threshold : float
+        correlation threshold, will remove one of pairs of features with a
+        correlation greater than this value.
+    remove_negative: Boolean
+        If true then features which are highly negatively correlated will
+        also be returned for removal.
+    Returns
+    --------
+    select_flat : list
+        listof column names to be removed
+    """
+    import numpy as np
+    corr_mat = data.corr()
+    if remove_negative:
+        corr_mat = np.abs(corr_mat)
+    corr_mat.loc[:, :] = np.tril(corr_mat, k=-1)
+    already_in = set()
+    result = []
+    for col in corr_mat:
+        perfect_corr = corr_mat[col][corr_mat[col] > threshold].index.tolist()
+        if perfect_corr and col not in already_in:
+            already_in.update(set(perfect_corr))
+            perfect_corr.append(col)
+            result.append(perfect_corr)
+    select_nested = [f[1:] for f in result]
+    select_flat = [i for j in select_nested for i in j]
+    return set(select_flat)
